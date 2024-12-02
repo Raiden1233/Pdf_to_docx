0d6d06c8c8985410a4cfd91686c6f893aa189d4e
Raiden <86716536+Raiden1233@users.noreply.github.com>
2024-12-02 19:15:47 +0530

    Update pdf-to-docx.py
    Added some error handling

--- pdf-to-docx.py
+++ pdf-to-docx.py
@@ -1,56 +1,42 @@
-# print((6*10)+(10+10))
+
 
 from pdf2docx import Converter
 import time
 
-def main():
+def main(pdf_path, docx_path):
 
-    pdf_file_path_input = input("Enter the pdf file path\nExample path:'E:\\important.pdf'\n\n--> ")
+
+    pdf_file_path_input = pdf_path
 
     if pdf_file_path_input.endswith(".pdf"):
 
         pdf_file_path_confirmed = pdf_file_path_input.replace("\\", "\\")
 
-        confirmation_for_docx_filename = input("Do you want to give custome name for docx file? yes|no --> ")
-
-        if confirmation_for_docx_filename != 'yes':
         
-            print("\ncontinuing with default option..\n")
-            print(f"Path: {pdf_file_path_confirmed.replace('.pdf', '.docx')}\n")
+        docx_custome_file_path = docx_path
+         
+        if docx_custome_file_path.endswith(".docx") != True:
+           
+            
+            docx_custome_file_path_fixed = docx_custome_file_path + ".docx"
+            print(f"\nPath confirmed! \nPath: {docx_custome_file_path_fixed}")
+
             time.sleep(2)
 
-            docx_file_path = pdf_file_path_confirmed
-            docx_file_path_conf = docx_file_path.replace(".pdf", ".docx")
+            
             conv = Converter(pdf_file_path_confirmed)
-            conv.convert(docx_file_path_conf)
+            conv.convert(docx_custome_file_path_fixed)
             conv.close()
         else:
 
-            docx_custome_file_path = input("Enter the docx file path\n Example path: 'D:\\document.docx'\n --> ")
-        
-         
-            if docx_custome_file_path.endswith(".docx") != True:
-           
-            
-                docx_custome_file_path_fixed = docx_custome_file_path + ".docx"
-                print(f"\nPath confirmed! \nPath: {docx_custome_file_path_fixed}")
-
-                time.sleep(2)
-
-            
-                conv = Converter(pdf_file_path_confirmed)
-                conv.convert(docx_custome_file_path_fixed)
-                conv.close()
-            else:
-
-                print(f"\nPATH CONFIRMED:\nPath: {docx_custome_file_path}\n")
+            print(f"\nPATH CONFIRMED:\nPath: {docx_custome_file_path}\n")
             
             
-                time.sleep(1)
-                docx_file_path = docx_custome_file_path
-                conv = Converter(pdf_file_path_confirmed)
-                conv.convert(docx_custome_file_path)
-                conv.close()
+            time.sleep(1)
+            docx_file_path = docx_custome_file_path
+            conv = Converter(pdf_file_path_confirmed)
+            conv.convert(docx_custome_file_path)
+            conv.close()
 
     
     else:
@@ -58,4 +44,51 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    
+
+
+    pdf_count = input("How many pdfs you want to convert? (Max 10 is recommended.)\n--> ")
+    
+    list_of_pdf_file_paths = []
+    list_of_docx_file_paths = []
+
+    if int(pdf_count) > 1:
+
+        try:
+            for i in range(int(pdf_count)):
+            
+                pdf_file_path = input("Enter the absolute path of .pdf file path\nExample path:'E:\\important.pdf'\n\n--> ")
+                    
+                # checking if the file within the path even exist.
+                if pdf_file_path != pdf_file_path + ".pdf":
+                    print("Please provide the .pdf file along withing path. Try again!")
+                    break
+                
+                list_of_pdf_file_paths.append(pdf_file_path)
+        
+                document_path = input("\nEnter the absolute path for docx file\nExample path: 'D:\\document .docx' (if drive letter is not specified, current directory will be a path)\n--> ")
+        
+                list_of_docx_file_paths.append(document_path)
+
+            for i in range(int(pdf_count)):
+                main(list_of_pdf_file_paths[i], list_of_docx_file_paths[i])
+
+        except:
+            print("\nHINT: The script wouldn't work properly, if you don't provide .pdf file within your path.")
+    else:
+        try:
+
+    
+            one_pdf_file_path = input("Enter the absolute path of .pdf file path\nExample path:'E:\\important.pdf'\n\n--> ")
+            
+            if one_pdf_file_path != one_pdf_file_path + ".pdf":
+                print("Please provide the .pdf file along withing path. Try again!")
+
+            else:
+                one_document_path = input("\nEnter the absolute path for docx file\nExample path: 'D:\\document .docx' (if drive letter is not specified, current directory will be a path)\n--> ")
+
+            main(one_pdf_file_path, one_document_path)
+        
+        except:
+            print("\nHINT: The script wouldn't work properly, if you don't provide .pdf file within your path.")
+
